c92f5b1073c0726fa1e5fd970f57e3f8f2e4227f
HoreaM <horea_manita@yahoo.com>
2017-11-15 14:14:55 +0200

    first cel attempt

--- price_monitor/spiders/cel.py
+++ price_monitor/spiders/cel.py
@@ -1,15 +1,19 @@
 import scrapy
 
+from price_monitor.items import PriceMonitorItem
+
 
 class CelSpider(scrapy.Spider):
     name = "cel.ro"
+    allowed_domains = ["cel.ro"]
     start_urls = ['http://www.cel.ro/cauta/placi-video/gtx+1070/0c-1']
 
     def parse(self, response):
-        item = response.meta.get('item', {})
-        '''item = {"title" : "", "price" : "", "url" : ""}'''
+        items = []
         for product in response.css('div.productListing-nume'):
+            item = PriceMonitorItem()
             item['title'] = product.css(".product_name span::text").extract_first("").strip()
-            item['url'] = product.css(".product_name::attr(href)").extract_first("").strip()
+            item['link'] = product.css(".product_name::attr(href)").extract_first("").strip()
             item['price'] = product.css('.pret_n b ::text').extract_first()
-            yield item
+            items.append(item)
+        return items
